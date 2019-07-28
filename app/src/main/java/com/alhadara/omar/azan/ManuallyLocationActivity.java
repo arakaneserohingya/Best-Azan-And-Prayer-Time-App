59bc9e2e05a166074e3c4bcd8d8e392f582d32b5
Omar Alkattan <oalkattan@outlook.com>
2019-07-28 14:45:27 +0300

    fix, ScrollView replaced with RecyclerView in:  -ManuallyLocationActivity  -ManuallyLocationCitiesActivity

--- app/src/main/java/com/alhadara/omar/azan/ManuallyLocationActivity.java
+++ app/src/main/java/com/alhadara/omar/azan/ManuallyLocationActivity.java
@@ -1,56 +1,66 @@
 package com.alhadara.omar.azan;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.Path;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Size;
-import android.view.Gravity;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.omar.azanapkmostafa.R;
 import com.opencsv.CSVReader;
 
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ManuallyLocationActivity extends AppCompatActivity {
 
+    private List<TextView> countries ;
+    private List<TextView> visibleCountries;
+    private RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manually_location);
-        addCountriesList("");
+
+        readListFromCSV();
+
+        recyclerView = findViewById(R.id.manually_location_activity_countries_layout);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new CountriesRecyclerViewAdapter(visibleCountries));
+
+
         final EditText search = findViewById(R.id.manually_location_activity_search);
         search.addTextChangedListener(new TextWatcher() {
             @Override public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                addCountriesList(search.getText().toString());
+                updateRecycelerView(search.getText().toString());
             }
             @Override public void afterTextChanged(Editable editable) {}
         });
     }
 
-    private void addCountriesList(String countriesContainString) {
-        LinearLayout layout = findViewById(R.id.manually_location_activity_countries_layout);
-        layout.removeAllViews();
-        countriesContainString=countriesContainString.toLowerCase();
-        LinearLayout.LayoutParams dividerParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                1);
+    private void updateRecycelerView(String visibleCountriesString) {
+        String s = visibleCountriesString.toLowerCase();
+        visibleCountries.clear();
+        for(int i=0;i<countries.size();i++) if(countries.get(i).getText().toString().toLowerCase().contains(s))
+            visibleCountries.add(countries.get(i));
+        recyclerView.getAdapter().notifyDataSetChanged();
+    }
+
+    private void readListFromCSV() {
+        countries = new ArrayList<>();
+        visibleCountries = new ArrayList<>();
         TextView tx;
-        View divider;
+
         final Intent intent = new Intent(this,ManuallyLocationCitiesActivity.class);
         try {
             CSVReader reader = new CSVReader(new InputStreamReader(getApplicationContext().getAssets().open("location_table_countries.csv")));
@@ -58,32 +68,24 @@ public class ManuallyLocationActivity extends AppCompatActivity {
             nextLine = new String[2];
             while ((nextLine = reader.readNext()) != null) {
                 // nextLine[] is an array of values from the line
-                if(!nextLine[1].toLowerCase().contains(countriesContainString)) continue;
-                tx = new TextView(this);
-                tx.setPadding(15,15,15,15);
-                tx.setTextColor(Color.WHITE);
-                tx.setGravity(Gravity.LEFT);
-                //tx.setLayoutParams(layoutParams);
-                tx.setText(nextLine[1]);
-                tx.setTextSize(15);
-                layout.addView(tx);
-                divider = new View(this);
-                divider.setLayoutParams(dividerParams);
-                divider.setBackgroundColor(Color.DKGRAY);
-                layout.addView(divider);
-                final int countryIndex = Integer.parseInt(nextLine[0]);
+                final int countryCode = Integer.parseInt(nextLine[0]);
                 final String countryName = nextLine[1];
+                tx = new TextView(this);
+                tx.setText(nextLine[1]);
                 tx.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        intent.putExtra("COUNTRY_INDEX",countryIndex);
+                        intent.putExtra("COUNTRY_INDEX",countryCode);
                         intent.putExtra("COUNTRY_NAME",countryName);
                         startActivity(intent);
                     }
                 });
+                countries.add(tx);
+                visibleCountries.add(tx);
             }
         } catch (IOException e) {
 
         }
     }
+
 }
--- app/src/main/java/com/alhadara/omar/azan/ManuallyLocationCitiesActivity.java
+++ app/src/main/java/com/alhadara/omar/azan/ManuallyLocationCitiesActivity.java
@@ -1,52 +1,64 @@
 package com.alhadara.omar.azan;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.omar.azanapkmostafa.R;
 import com.opencsv.CSVReader;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ManuallyLocationCitiesActivity extends AppCompatActivity {
 
+    private List<TextView> cities ;
+    private List<TextView> visibleCities;
+    private RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manually_location_cities);
-        addCitiesList("");
+
+        readListFromCSV();
+
+        recyclerView = findViewById(R.id.manually_location_cities_activity_countries_layout);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new CountriesRecyclerViewAdapter(visibleCities));
+
         final EditText search = findViewById(R.id.manually_location_cities_activity_search);
         search.addTextChangedListener(new TextWatcher() {
             @Override public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                addCitiesList(search.getText().toString());
+                updateRecycelerView(search.getText().toString());
             }
             @Override public void afterTextChanged(Editable editable) {}
         });
     }
 
-    private void addCitiesList(String citiesContainString) {
-        LinearLayout layout = findViewById(R.id.manually_location_cities_activity_countries_layout);
-        layout.removeAllViews();
-        LinearLayout.LayoutParams dividerParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                1);
-        TextView tx;
-        View divider;
-        String countryName = getIntent().getExtras().getString("COUNTRY_NAME");
 
+    private void updateRecycelerView(String visibleCitiesString) {
+        String s = visibleCitiesString.toLowerCase();
+        visibleCities.clear();
+        for(int i=0;i<cities.size();i++) if(cities.get(i).getText().toString().toLowerCase().contains(s))
+            visibleCities.add(cities.get(i));
+        recyclerView.getAdapter().notifyDataSetChanged();
+    }
+    private void readListFromCSV() {
+        cities = new ArrayList<>();
+        visibleCities = new ArrayList<>();
+        TextView tx;
+        String countryName = getIntent().getExtras().getString("COUNTRY_NAME");
         try {
             CSVReader reader = new CSVReader(new InputStreamReader(getApplicationContext().getAssets().open("location_table_"+countryName.charAt(0)+".csv")));
             String[] nextLine;
@@ -55,19 +67,10 @@ public class ManuallyLocationCitiesActivity extends AppCompatActivity {
             while ((nextLine = reader.readNext()) != null) {
                 // nextLine[] is an array of values from the line
                 if(!nextLine[0].equals(countryName)) break;
-                if(!nextLine[1].toLowerCase().contains(citiesContainString.toLowerCase())) continue;
                 tx = new TextView(this);
-                tx.setPadding(15,15,15,15);
-                tx.setTextColor(Color.WHITE);
-                tx.setGravity(Gravity.LEFT);
-                //tx.setLayoutParams(layoutParams);
-                tx.setText( nextLine[1]);
-                tx.setTextSize(15);
-                layout.addView(tx);
-                divider = new View(this);
-                divider.setLayoutParams(dividerParams);
-                divider.setBackgroundColor(Color.DKGRAY);
-                layout.addView(divider);
+                tx.setText(nextLine[1]);
+                cities.add(tx);
+                visibleCities.add(tx);
             }
         } catch (IOException e) {
 
